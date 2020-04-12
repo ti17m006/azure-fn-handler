09849145c60886a45748bce233c70714a5110c27
ti17m006 <ti17m006@technikum-wien.at>
2020-04-12 17:49:57 +0200

    dummy-a dummy value

--- Requester/index.js
+++ Requester/index.js
@@ -3,31 +3,36 @@ const http = require('http');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    http.get('someUrl', function (success) {
-        if (success) {
-            http.get('someOtherUrl', function (array) {
-                array.forEach(element => {
-                    element.http.get('someAPI')
-                        .then(() => {
-                            context.res = {
-                                status: 200,
-                                body: "Success"
-                            };
-                        })
-                        .catch(() => {
-                            context.res = {
-                                status: 400,
-                                body: "An error has occured"
-                            };
-                        });
-                });
-            });
-        }
-    }).on("error", (error) => {
-        console.log(`Error: ${error.message}`);
-        context.res = {
-            status: 400,
-            body: "An error has occured"
-        };
-    });
+
 };
+
+/** Draft */
+/**
+http.get('someUrl', function (success) {
+    if (success) {
+        http.get('someOtherUrl', function (array) {
+            array.forEach(element => {
+                element.http.get('someAPI')
+                    .then(() => {
+                        context.res = {
+                            status: 200,
+                            body: "Success"
+                        };
+                    })
+                    .catch(() => {
+                        context.res = {
+                            status: 400,
+                            body: "An error has occured"
+                        };
+                    });
+            });
+        });
+    }
+}).on("error", (error) => {
+    console.log(`Error: ${error.message}`);
+    context.res = {
+        status: 400,
+        body: "An error has occured"
+    };
+});
+ */
