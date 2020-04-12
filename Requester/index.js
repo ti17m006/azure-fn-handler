c6ff7501dfad50ce5361a78b4c6ded04510a41f0
ti17m006 <ti17m006@technikum-wien.at>
2020-04-12 17:59:37 +0200

    requester dummy-a value test

--- Requester/index.js
+++ Requester/index.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
+const dummy_a_url = "https://req-handler.azurewebsites.net/microsoft/graph/dummy-a";
+const dummy_a_query_code = "?code=N7u0QKE/VtpqK3RjZjygSzGWk1aWWJWCvZn3t5WMCXHczBDJmWYtKA==";
+
+const dummy_a = dummy_a_url + dummy_a_query_code;
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-
+    http.get(dummy_a, (output) => {
+        context.res = {
+            body: output.url
+        };
+    });
 };
 
 /** Draft */
