1461bf38ab05e210b7c6325832bcffadff3c73ee
ti17m006 <ti17m006@technikum-wien.at>
2020-04-12 19:01:56 +0200

    requester dummy-a value test

--- Requester/index.js
+++ Requester/index.js
@@ -1,20 +1,22 @@
 const http = require('http');
 const url = require('url');
 
-const df = require("durable-functions");
+const fn_url = "https://req-handler.azurewebsites.net/microsoft/graph/";
 
-const dummy_a_url = "https://req-handler.azurewebsites.net/microsoft/graph/dummy-a";
-const dummy_a_query_code = "?code=N7u0QKE/VtpqK3RjZjygSzGWk1aWWJWCvZn3t5WMCXHczBDJmWYtKA==";
-const dummy_a = dummy_a_url + dummy_a_query_code;
+const dummy_a_query_code = "dummy-a?code=N7u0QKE/VtpqK3RjZjygSzGWk1aWWJWCvZn3t5WMCXHczBDJmWYtKA==";
+const dummy_a = fn_url + dummy_a_query_code;
 
 module.exports = async function (context, req) {
     // context.log('JavaScript HTTP trigger function processed a request.');
 
-    const t = yield context.df.callActivity("dummy-a")
+
     context.res = {
-        body: t.body
+        body: "t.body"
     };
 };
+
+// const df = require("durable-functions");
+// const t = yield context.df.callActivity("dummy-a")
 
 /** Draft */
 /**
