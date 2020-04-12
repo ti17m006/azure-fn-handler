b062af533debb5c261342d6faf84ef126062924c
ti17m006 <ti17m006@technikum-wien.at>
2020-04-12 18:51:34 +0200

    requester dummy-a value test

--- Requester/index.js
+++ Requester/index.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const url = require('url');
 
+const df = require("durable-functions");
+
 const dummy_a_url = "https://req-handler.azurewebsites.net/microsoft/graph/dummy-a";
 const dummy_a_query_code = "?code=N7u0QKE/VtpqK3RjZjygSzGWk1aWWJWCvZn3t5WMCXHczBDJmWYtKA==";
 const dummy_a = dummy_a_url + dummy_a_query_code;
@@ -8,7 +10,7 @@ const dummy_a = dummy_a_url + dummy_a_query_code;
 module.exports = async function (context, req) {
     // context.log('JavaScript HTTP trigger function processed a request.');
 
-    const query = dummy_a.query;
+    const t = yield context.df.callActivity("dummy-a")
     context.res = {
         body: query
     };
