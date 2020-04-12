e17d07975a88f14c536986f11444b2f9412788e3
ti17m006 <ti17m006@technikum-wien.at>
2020-04-12 15:37:52 +0200

    Hello/index - apiUrl added

--- HttpTriggerHello/index.js
+++ HttpTriggerHello/index.js
@@ -1,12 +1,14 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     console.log('Hello Azure');
+    const apiUrl = "https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-http-webhook-trigger?tabs=javascript";
 
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
             /** get req body -> JSON format */
-            body: "Hello " + req.query.name + " " + req.body.name
+
+            body: "Hello " + req.query.name + " " + req.body.name + " " + apiUrl
         };
     }
     else {
