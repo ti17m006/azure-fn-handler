1911b9ccda2f97a58d2de2a85c092a37b7d3eb04
ti17m006 <ti17m006@technikum-wien.at>
2020-04-12 14:49:09 +0200

    initial dummy functions added

--- dummy-b/index.js
+++ dummy-b/index.js
@@ -1,18 +1,16 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    console.log('Hello Azure');
 
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            /** get req body -> JSON format */
-            body: "Hello " + req.query.name + " " + req.body.name
+            body: "Hello " + (req.query.name || req.body.name)
         };
     }
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body and :)"
+            body: "Please pass a name on the query string or in the request body"
         };
     }
 };
